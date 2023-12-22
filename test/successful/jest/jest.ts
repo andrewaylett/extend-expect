5c52a9d88aafb7deba90f590099744f5b044af89
Andrew Aylett <andrew@aylett.co.uk>
2023-12-22 17:31:12 +0000

    Allow passing in an extended expect
    This helps us support Jest's extensions without depending directly on
    Jest.
    
    Also use downgrade-build instead of re-implementing it.
    
    Signed-off-by: Andrew Aylett <andrew@aylett.co.uk>

---
+++ test/successful/jest/jest.ts
@@ -0,0 +1,37 @@
+import { extend } from 'extend-expect';
+import { expect as jestExpect } from '@jest/globals';
+
+import type {
+    MatchersFor,
+    MatcherState,
+    ExpectationResult,
+} from 'extend-expect';
+
+async function toBeAStub(
+    this: MatcherState,
+    received: unknown,
+    expected: unknown,
+): Promise<ExpectationResult> {
+    return {
+        pass: true,
+        message: () => '',
+    };
+}
+
+interface DummyMatchers {
+    toBeAStub(success?: boolean): Promise<void>;
+}
+
+const processMatchers: MatchersFor<DummyMatchers> = {
+    toBeAStub,
+};
+
+const expect = extend(processMatchers, jestExpect);
+
+jestExpect(1).toMatchInlineSnapshot('1');
+// Only available in Jest's extensions
+expect(1).toMatchInlineSnapshot('1');
+// Locally extended
+expect(2).toBeAStub(false);
+
+export default expect;
